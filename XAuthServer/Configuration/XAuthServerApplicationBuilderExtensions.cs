3ffcf5a3e85261c269a6822e957696ce0e378a91
avegarajucbl <a.vegaraju@coolblue.nl>
2017-05-30 00:59:04 +0200

    Add project files.

---
+++ XAuthServer/Configuration/XAuthServerApplicationBuilderExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+using XAuthServer.Middleware;
+
+namespace XAuthServer.Configuration
+{
+    public static class XAuthServerApplicationBuilderExtensions
+    {
+        public static IApplicationBuilder UseXAuthServer(this IApplicationBuilder applicationBuilder)
+        {
+            applicationBuilder.UseMiddleware<RequestBuilderMiddleware>();
+
+            return applicationBuilder;
+        }
+    }
+}
---
+++ XAuthServer/Middleware/RequestBuilderMiddleware.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Http;
+
+namespace XAuthServer.Middleware
+{
+    internal class RequestBuilderMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public RequestBuilderMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public async Task Invoke(HttpContext context)
+        {
+            var request = context.Request;
+
+            var userId = request.HttpContext.User.GetUserId();
+            
+            await _next(context);
+        }
+    }
+
+    public static class ClaimsPrincipalExtensions
+    {
+        public static string GetUserId(this ClaimsPrincipal principal)
+        {
+            if (principal == null)
+                throw new ArgumentNullException(nameof(principal));
+
+            return principal.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        }
+    }
+}
